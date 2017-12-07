0cbaf1ca0cdf977e7e2501e25ed8d684d4ad2965
Christopher Slater <chris@snapper.fish>
2017-12-07 07:58:25 +0000

    Remove build folder

--- build/routes/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var hello_1 = require("./hello");
-var users_1 = require("./users");
-exports.routes = [].concat(users_1.routes, hello_1.routes);
-//# sourceMappingURL=index.js.map
--- build/routes/users.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.routes = [];
-// routes.push({
-// })
-//# sourceMappingURL=users.js.map
